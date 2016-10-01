5fa227bda5d20a255ced6edd8d9522b9255eb488
Kelana Latimer <Kelana Latimer>
2016-10-01 12:13:12 -0500

    updating liri.js

---
+++ liri.js
@@ -0,0 +1,27 @@
+var request = require('request');
+var Twitter = require('twitter');
+var spotify = require('spotify');
+
+var client = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+});
+
+var params = {screen_name: 'nodejs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+  // Do something with tweets.
+});
+
+spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+
+    // Do something with 'data'
+});
